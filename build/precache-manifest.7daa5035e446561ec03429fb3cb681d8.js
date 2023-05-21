cd7d475d7b92db329df8e764e875ca27b71bd0db
cdtello <carlos.tello@correounivalle.edu.co>
2023-05-21 14:59:04 -0500

    first commit

---
+++ build/precache-manifest.7daa5035e446561ec03429fb3cb681d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "649e8fa5f35b050c5454",
+    "url": "/static/css/main.312dee4a.chunk.css"
+  },
+  {
+    "revision": "649e8fa5f35b050c5454",
+    "url": "/static/js/main.649e8fa5.chunk.js"
+  },
+  {
+    "revision": "6d8775655d1695e29e07",
+    "url": "/static/js/1.6d877565.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "52355593e805f6c172bfdc156a39a031",
+    "url": "/static/media/Inter-Regular.52355593.ttf"
+  },
+  {
+    "revision": "431882308d0910d292648753b2b2880b",
+    "url": "/static/media/Inter-Medium.43188230.ttf"
+  },
+  {
+    "revision": "67b2d54ebf6d61853f2bc5c201e81a45",
+    "url": "/static/media/Inter-SemiBold.67b2d54e.ttf"
+  },
+  {
+    "revision": "edbd837c535244f5e30f8c1bcf18a209",
+    "url": "/index.html"
+  }
+];
