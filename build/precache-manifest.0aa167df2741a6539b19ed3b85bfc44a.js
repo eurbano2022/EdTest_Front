e12e72b0851638e545d6ee0c1407335df04ff701
cdtello <carlos.tello@correounivalle.edu.co>
2023-06-10 15:18:20 -0500

    test wordings

--- build/precache-manifest.0aa167df2741a6539b19ed3b85bfc44a.js
+++ build/precache-manifest.0aa167df2741a6539b19ed3b85bfc44a.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "587bfadf2b7e8b427186",
+    "revision": "072e4d160b1596d57a1c",
     "url": "/static/css/main.89c87238.chunk.css"
   },
   {
-    "revision": "587bfadf2b7e8b427186",
-    "url": "/static/js/main.587bfadf.chunk.js"
+    "revision": "072e4d160b1596d57a1c",
+    "url": "/static/js/main.072e4d16.chunk.js"
   },
   {
     "revision": "9309ae84c071046b617a",
@@ -20,15 +20,15 @@ self.__precacheManifest = [
     "url": "/static/media/Inter-Regular.52355593.ttf"
   },
   {
-    "revision": "67b2d54ebf6d61853f2bc5c201e81a45",
-    "url": "/static/media/Inter-SemiBold.67b2d54e.ttf"
-  },
-  {
     "revision": "431882308d0910d292648753b2b2880b",
     "url": "/static/media/Inter-Medium.43188230.ttf"
   },
   {
-    "revision": "66808d18843b30d79aff8123bfbed4f6",
+    "revision": "67b2d54ebf6d61853f2bc5c201e81a45",
+    "url": "/static/media/Inter-SemiBold.67b2d54e.ttf"
+  },
+  {
+    "revision": "78ee0cf549931bb0c1721cf42bbea764",
     "url": "/index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.41f404b9accebb8da63dfc0f35741490.js"
+  "/precache-manifest.0aa167df2741a6539b19ed3b85bfc44a.js"
 );
 
 workbox.clientsClaim();
