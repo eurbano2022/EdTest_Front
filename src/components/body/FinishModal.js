1c3e688f899cc05b12ef7b758b8c4b035e856cc2
cdtello <carlos.tello@correounivalle.edu.co>
2023-07-09 18:23:27 -0500

    format all pages

--- src/components/body/FinishModal.js
+++ src/components/body/FinishModal.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import ReactModal from 'react-modal';
 import { Button, Form } from 'react-bootstrap';
 import '../../styles/FinishModal.css';
-import SpiderChart from 'react-spider-chart';
 
 function FinishModal(props) {
     const [name, setName] = useState('');
