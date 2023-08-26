f049df280c5f05f56b7326c6dcf1e1e26d971ac9
CTelloMeli <carlos.telloruiz@mercadolibre.com.co>
2023-08-26 17:01:32 -0500

    test

--- src/plugins/backend_request.js
+++ src/plugins/backend_request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export class BackendRequest {
-    constructor(token, baseURL='http://172.31.32.126/api/v1') {
+    constructor(token, baseURL='http://localhost/api/v1') {
         this.__baseURL = baseURL
         this.__token = token
         this.__axios = axios
